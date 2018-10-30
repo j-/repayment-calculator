a73e522cd560baf8368c0d0a663d77bd892cac1b
j- <j@skeoh.com>
2018-10-31 09:12:47 +1100

    Make input fields editable

--- src/containers/InterestRateInput.tsx
+++ src/containers/InterestRateInput.tsx
@@ -1,20 +1,25 @@
 import { connect, MapStateToProps, MapDispatchToProps, MergeProps } from 'react-redux';
 import Input, { Props as P } from '../components/Input';
 import { RootReducerState, getInterestRateAnnualPercentage } from '../store';
+import { setInterestRate } from 'src/store/actions';
 
 const mapStateToProps: MapStateToProps<P, P, RootReducerState> = (state) => ({
 	value: getInterestRateAnnualPercentage(state),
 });
 
-const mapDispatchToProps: MapDispatchToProps<P, P> = ({
-
+const mapDispatchToProps: MapDispatchToProps<P, P> = (dispatch) => ({
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => (
+		dispatch(
+			setInterestRate(Number(e.currentTarget.value))
+		)
+	),
 });
 
 const mergeProps: MergeProps<P, P, P, P> = (state, dispatch, own) => ({
 	...state,
 	...dispatch,
 	...own,
-	readOnly: true,
+	type: 'number',
 });
 
 export default connect(
--- src/containers/LoanTermInput.tsx
+++ src/containers/LoanTermInput.tsx
@@ -1,20 +1,25 @@
 import { connect, MapStateToProps, MapDispatchToProps, MergeProps } from 'react-redux';
 import Input, { Props as P } from '../components/Input';
 import { RootReducerState, getLoanTerm } from '../store';
+import { setLoanTerm } from 'src/store/actions';
 
 const mapStateToProps: MapStateToProps<P, P, RootReducerState> = (state) => ({
 	value: getLoanTerm(state),
 });
 
-const mapDispatchToProps: MapDispatchToProps<P, P> = ({
-
+const mapDispatchToProps: MapDispatchToProps<P, P> = (dispatch) => ({
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => (
+		dispatch(
+			setLoanTerm(Number(e.currentTarget.value))
+		)
+	),
 });
 
 const mergeProps: MergeProps<P, P, P, P> = (state, dispatch, own) => ({
 	...state,
 	...dispatch,
 	...own,
-	readOnly: true,
+	type: 'number',
 });
 
 export default connect(
