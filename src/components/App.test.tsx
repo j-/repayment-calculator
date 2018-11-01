a5110728db741f6b2de08d01386121b07ed82e7f
j- <j@skeoh.com>
2018-11-01 16:23:22 +1100

    Fix broken app component test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<App />, div);
-	ReactDOM.unmountComponentAtNode(div);
+	expect(() => {
+		shallow(<App />);
+	}).not.toThrow();
 });
